90ceddf451b889db4842a3ea280eaccb6fc095b7
Jakob Jul Elben <elbenjakobjul@gmail.com>
2021-06-03 17:33:20 +0200

    Add helm repository

--- scripts/test.sh
+++ scripts/test.sh
@@ -2,6 +2,10 @@
 
 set -e
 
+// Helm repositories 
+helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx
+
+// Testing
 skaffold build --profile development --file-output /tmp/build.json
 skaffold deploy --build-artifacts /tmp/build.json
 skaffold test --build-artifacts /tmp/build.json
