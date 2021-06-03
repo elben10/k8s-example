858eb88896dd7724820f78b91cc6a42fb66dbe11
Jakob Jul Elben <elbenjakobjul@gmail.com>
2021-06-03 17:47:02 +0200

    ..

--- scripts/test.sh
+++ scripts/test.sh
@@ -8,5 +8,7 @@ helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx
 # Testing
 skaffold build --profile development --file-output /tmp/build.json
 skaffold deploy --build-artifacts /tmp/build.json
+kubectl describe deployments
+kubectl describe pods
 skaffold test --build-artifacts /tmp/build.json
 skaffold delete
